af665e920ccd0ae2fd63beb1fbe31187ba2b5868
Darwin Nacionales <darwin_nacionales@yahoo.com>
2023-01-27 18:29:59 +0800

    Refactored

--- public/script.js
+++ public/script.js
@@ -7,8 +7,14 @@ const myPeer = new Peer(undefined, {
     port: '3001'
 });
 
+myPeer.on("open", id => {
+    socket.emit("join-room", ROOM_ID, id);
+});
+
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+const peers = {};
 
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -25,32 +31,12 @@ navigator.mediaDevices.getUserMedia({
         })
 
         peers[call.peer] = call
-    })
+    });
 
     socket.on("user-connected", userId => {
         connectToNewUser(userId, stream);
     })
 })
-
-socket.on("user-disconnected", userId => {
-    if (peers[userId]) {
-        peers[userId].close();
-    }
-})
-
-myPeer.on("open", id => {
-    socket.emit("join-room", ROOM_ID, id);
-});
-
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-        video.play();
-    })
-    videoGrid.append(video);
-}
-
-const peers = {};
 
 function connectToNewUser(userId, stream) {
     const call = myPeer.call(userId, stream);
@@ -64,3 +50,17 @@ function connectToNewUser(userId, stream) {
 
     peers[userId] = call;
 }
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}
+
+socket.on("user-disconnected", userId => {
+    if (peers[userId]) {
+        peers[userId].close();
+    }
+})
